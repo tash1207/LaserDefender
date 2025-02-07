da5cf60e192b54ec67d80c9209e96b64cfa7f9a8
Tasha Hankewych <thankewych@gmail.com>
2025-02-07 00:00:04 -0600

    Constrain player movement to the bounds of the screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,38 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 7f;
     Vector2 moveInput;
 
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     void Move()
     {
-        Vector3 delta = moveInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = moveInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
